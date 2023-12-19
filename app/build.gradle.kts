78fbecaf60673addce203dcba43fa3b92ae43216
Flexo <twinklebot@plshark.net>
2023-12-19 06:01:06 +0000

    Update dependency com.google.guava:guava to v33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ dependencyManagement {
     }
     dependencies {
         dependency("io.mockk:mockk:1.13.8")
-        dependency("com.google.guava:guava:32.1.3-jre")
+        dependency("com.google.guava:guava:33.0.0-jre")
     }
 }
 
