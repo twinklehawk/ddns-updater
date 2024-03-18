01749fad3ee8289836971e3d67ee1b079a2a8393
Flexo <twinklebot@plshark.net>
2024-03-18 14:01:14 +0000

    Update plugin org.jmailen.kotlinter to v4.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("org.springframework.boot") version "3.2.3"
     jacoco
     id("io.gitlab.arturbosch.detekt") version "1.23.5"
-    id("org.jmailen.kotlinter") version "4.2.0"
+    id("org.jmailen.kotlinter") version "4.3.0"
 }
 
 repositories {
