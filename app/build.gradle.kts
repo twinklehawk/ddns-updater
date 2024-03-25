d10ec7b3c7e11f15f90c53aeea36aac8d52889e8
Flexo <twinklebot@plshark.net>
2024-03-25 11:01:31 +0000

    Update plugin io.gitlab.arturbosch.detekt to v1.23.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
     id("org.springframework.boot") version "3.2.3"
     jacoco
-    id("io.gitlab.arturbosch.detekt") version "1.23.5"
+    id("io.gitlab.arturbosch.detekt") version "1.23.6"
     id("org.jmailen.kotlinter") version "4.3.0"
 }
 
