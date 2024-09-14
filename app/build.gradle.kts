48a4f06a91499fc214ec01f0b7ca2177a7d3a34c
Flexo <twinklebot@plshark.net>
2024-09-14 04:07:04 +0000

    Update dependency org.jetbrains.kotlinx:kotlinx-coroutines-bom to v1.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ dependencyManagement {
     imports {
         mavenBom("org.springframework.boot:spring-boot-dependencies:3.3.3")
         mavenBom("org.jetbrains.kotlin:kotlin-bom:2.0.20")
-        mavenBom("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.8.1")
+        mavenBom("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.9.0")
     }
     dependencies {
         dependency("io.mockk:mockk:1.13.12")
