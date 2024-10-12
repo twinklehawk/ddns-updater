753c6a9a6040c83d20389e650bd590ecad529a22
Flexo <twinklebot@plshark.net>
2024-10-12 06:03:03 +0000

    Update plugin org.jetbrains.kotlin.jvm to v2.0.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.0.20"
+    kotlin("jvm") version "2.0.21"
     kotlin("plugin.spring") version "2.0.21"
     id("io.spring.dependency-management") version "1.1.6"
     id("org.springframework.boot") version "3.3.4"
