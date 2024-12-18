1d13cc29bd4e2dad73f295aa06460efb0c0fcfa2
Flexo <136768647+twinklehawk-bot@users.noreply.github.com>
2024-12-17 23:03:39 -0500

    Update plugin io.spring.dependency-management to v1.1.7 (#148)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.spring") version "2.1.0"
-    id("io.spring.dependency-management") version "1.1.6"
+    id("io.spring.dependency-management") version "1.1.7"
     id("org.springframework.boot") version "3.4.0"
     jacoco
     id("io.gitlab.arturbosch.detekt") version "1.23.7"
